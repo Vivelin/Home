f71ecb4b7b0b8c6e2ac9e581800e53b2402273b0
Ruben Verdoes <vivelin@outlook.com>
2017-04-21 22:42:41 +0200

    Add quote, remove empty <p> tags and allow HTML in cite for linking source

--- Vivelin.Home/Data/HomeContext.cs
+++ Vivelin.Home/Data/HomeContext.cs
@@ -19,7 +19,7 @@ namespace Vivelin.Home.Data
             var migrations = await Database.GetPendingMigrationsAsync();
             if (migrations.Any())
                 throw new InvalidOperationException("Unable to seed a database with pending migrations");
-
+            
             var hasQuotes = await Quotes.AnyAsync();
             if (!hasQuotes)
             {
@@ -32,7 +32,13 @@ namespace Vivelin.Home.Data
                 Quotes.Add(new Quote
                 {
                     Text = "Not a wise decision, but a decision nonetheless.",
-                    Citation = "Age of Mythology, taunt 37"
+                    Citation = "Age of Mythology, <a href='https://youtu.be/-EvhQcmNORo?t=85' target='_blank' rel='external'>taunt 37</a>"
+                });
+
+                Quotes.Add(new Quote
+                {
+                    Text = "<q>I had no idea who she is. I knew as much about myself and what I wanted as I do about how to squeeze a fruit in a grocery store and know if it's gonna be good in a meal. I still don't know; I'd pick up a lemon and squeeze it and I'm like <q>Well, it's definitely a lemon... 'cause there's a label here that says so... Guess we're gonna order out tonight.</q></q>",
+                    Citation = "Sean Plott, <a href='https://youtu.be/CdLnuGAPNUg?t=195' target='_blank' rel='external'>Day[9] Story Time #5 - A High School Crush</a>"
                 });
 
                 await SaveChangesAsync();
--- Vivelin.Home/TagHelpers/CommonMarkTagHelper.cs
+++ Vivelin.Home/TagHelpers/CommonMarkTagHelper.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace Vivelin.Home.TagHelpers
 {
-    [HtmlTargetElement(Attributes = nameof(CommonMark))]
+    [HtmlTargetElement(Attributes = "commonmark")]
     public class CommonMarkTagHelper : TagHelper
     {
         [HtmlAttributeName("commonmark")]
         public string CommonMark { get; set; }
 
-        public override void Process(TagHelperContext context, TagHelperOutput output)
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             var html = CommonMarkConverter.Convert(CommonMark);
-            output.Content.SetHtmlContent(html);
+            output.PreContent.SetHtmlContent(html);
         }
     }
 }
