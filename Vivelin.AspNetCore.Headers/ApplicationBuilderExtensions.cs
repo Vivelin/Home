993601bef52234fd96f5d7e357331c6464a633df
Ruben Verdoes <vivelin@outlook.com>
2018-05-10 12:54:06 +0200

    Refactor response headers into middleware

---
+++ Vivelin.AspNetCore.Headers/ApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+
+namespace Vivelin.AspNetCore.Headers
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseResponseHeaders(this IApplicationBuilder app, ResponseHeadersOptions options)
+        {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return app.UseMiddleware<ResponseHeadersMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ Vivelin.AspNetCore.Headers/ResponseHeadersOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Vivelin.AspNetCore.Headers
+{
+    public class ResponseHeadersOptions
+    {
+        public ResponseHeadersOptions()
+        {
+            Headers = new HeaderDictionary();
+        }
+
+        public IHeaderDictionary Headers { get; }
+    }
+}
